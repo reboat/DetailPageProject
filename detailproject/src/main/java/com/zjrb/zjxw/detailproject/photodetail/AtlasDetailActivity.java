3b2fda7c7fd72966bd926f54ba0211ee5e028a96
lixinke <lixink@8531.cn>
2017-11-10 17:42:10 +0800

    获取高度

--- detailproject/src/main/java/com/zjrb/zjxw/detailproject/photodetail/AtlasDetailActivity.java
+++ detailproject/src/main/java/com/zjrb/zjxw/detailproject/photodetail/AtlasDetailActivity.java
@@ -107,6 +107,8 @@ public class AtlasDetailActivity extends BaseActivity implements ViewPager
     FrameLayout mView;
     @BindView(R2.id.atlas_scrollView)
     ScrollView mScrollView;
+    @BindView(R2.id.atlas_container)
+    LinearLayout mBottomContainer;
 
 
     /**
@@ -361,7 +363,7 @@ public class AtlasDetailActivity extends BaseActivity implements ViewPager
             @Override
             public void onGlobalLayout() {
                 int titleHeight = mLyContainer.getMeasuredHeight()+mLyContainer.getPaddingBottom();
-                mMaxHeight = mScrollView.getMeasuredHeight();
+                mMaxHeight = mBottomContainer.getMeasuredHeight();
                 int defaultMin = (int) (titleHeight + (mTvContent.getLineHeight() + mTvContent.getLineSpacingExtra()) * 2.5);
                 mMinHeight = (titleHeight + mTvContent.getHeight()) < defaultMin ? titleHeight + mTvContent.getHeight() : defaultMin;
                 ViewGroup.LayoutParams paramsSc = mScrollView.getLayoutParams();
@@ -476,13 +478,6 @@ public class AtlasDetailActivity extends BaseActivity implements ViewPager
 
     @Override
     public void onPageSelected(int position) {
-
-        ViewGroup.LayoutParams params = mScrollView.getLayoutParams();
-        if (params != null) {
-            params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-            mScrollView.setLayoutParams(params);
-        }
-
         mIndex = position;
         mTvIndex.setText(String.valueOf(mIndex + 1) + "/");
         setSwipeBackEnable(0 == position);

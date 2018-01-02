a94c03a25411cfb867131c9f11e43bcf75648b5f
汪林杰 <wljie@8531.cn>
2018-01-02 17:04:24 +0800

    添加链接稿

--- detailproject/src/main/java/com/zjrb/zjxw/detailproject/link/LiveLinkActivity.java
+++ detailproject/src/main/java/com/zjrb/zjxw/detailproject/link/LiveLinkActivity.java
@@ -5,26 +5,33 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aliya.view.fitsys.FitWindowsFrameLayout;
+import com.aliya.view.fitsys.FitWindowsRelativeLayout;
 import com.trs.tasdk.entity.ObjectType;
 import com.zjrb.core.api.callback.APIExpandCallBack;
 import com.zjrb.core.common.base.BaseActivity;
 import com.zjrb.core.common.base.toolbar.TopBarFactory;
 import com.zjrb.core.common.base.toolbar.holder.DefaultTopBarHolder1;
+import com.zjrb.core.common.biz.TouchSlopHelper;
 import com.zjrb.core.common.global.IKey;
 import com.zjrb.core.common.global.RouteManager;
+import com.zjrb.core.domain.CommentDialogBean;
 import com.zjrb.core.nav.Nav;
 import com.zjrb.core.ui.UmengUtils.OutSizeAnalyticsBean;
 import com.zjrb.core.ui.UmengUtils.UmengShareBean;
 import com.zjrb.core.ui.UmengUtils.UmengShareUtils;
 import com.zjrb.core.ui.widget.ZBWebView;
+import com.zjrb.core.ui.widget.dialog.CommentWindowDialog;
 import com.zjrb.core.utils.T;
 import com.zjrb.core.utils.UIUtils;
 import com.zjrb.core.utils.click.ClickTracker;
@@ -50,14 +57,14 @@ import cn.daily.news.analytics.Analytics;
  * Created by wanglinjie.
  * create time:2017/10/08  上午10:14
  */
-public class BrowserLinkActivity extends BaseActivity implements View.OnClickListener {
+public class LiveLinkActivity extends BaseActivity implements View.OnClickListener, TouchSlopHelper.OnTouchSlopListener {
 
     @BindView(R2.id.web_view)
     ZBWebView mWebView;
-    //    @BindView(R2.id.tv_comment)
-//    TextView mTvComment;
-//    @BindView(R2.id.fl_comment)
-//    FrameLayout mFyContainer;
+    @BindView(R2.id.tv_comment)
+    TextView mTvComment;
+    @BindView(R2.id.fl_comment)
+    FrameLayout mFyContainer;
     @BindView(R2.id.menu_comment)
     ImageView mMenuComment;
     @BindView(R2.id.tv_comments_num)
@@ -66,14 +73,18 @@ public class BrowserLinkActivity extends BaseActivity implements View.OnClickLis
     ImageView mMenuPrised;
     @BindView(R2.id.menu_setting)
     ImageView mMenuSetting;
-    //    @BindView(R2.id.ly_bottom_comment)
-//    FitWindowsRelativeLayout mFloorBar;
+    @BindView(R2.id.ly_bottom_comment)
+    FitWindowsRelativeLayout mFloorBar;
     @BindView(R2.id.ry_container)
     FitWindowsFrameLayout mContainer;
     @BindView(R2.id.v_container)
     FrameLayout mView;
 
     private String mArticleId;
+    /**
+     * 上下滑动超出范围处理
+     */
+    private TouchSlopHelper mTouchSlopHelper;
     /**
      * 详情页数据
      */
@@ -88,10 +99,19 @@ public class BrowserLinkActivity extends BaseActivity implements View.OnClickLis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.module_detail_activity_browser_link);
+        setContentView(R.layout.module_detail_activity_browser);
         ButterKnife.bind(this);
         getIntentData(getIntent());
+        init();
         loadData();
+    }
+
+    /**
+     * 初始化滑动/webview
+     */
+    private void init() {
+        mTouchSlopHelper = new TouchSlopHelper();
+        mTouchSlopHelper.setOnTouchSlopListener(this);
     }
 
     private String mFromChannel;
@@ -163,7 +183,7 @@ public class BrowserLinkActivity extends BaseActivity implements View.OnClickLis
                 if (errCode == ErrorCode.DRAFFT_IS_NOT_EXISE) {
                     showEmptyNewsDetail();
                 } else {
-                    T.showShortNow(BrowserLinkActivity.this, errMsg);
+                    T.showShortNow(LiveLinkActivity.this, errMsg);
                 }
             }
         }).setTag(this).bindLoadViewHolder(replaceLoad(mContainer)).exe(mArticleId, mFromChannel);
@@ -173,7 +193,7 @@ public class BrowserLinkActivity extends BaseActivity implements View.OnClickLis
      * @param data 填充详情页数据
      */
     private void fillData(DraftDetailBean data) {
-//        mFloorBar.setVisibility(View.VISIBLE);
+        mFloorBar.setVisibility(View.VISIBLE);
         mView.setVisibility(View.GONE);
         // 记录阅读记录
         if (data != null && data.getArticle() != null) {
@@ -201,9 +221,11 @@ public class BrowserLinkActivity extends BaseActivity implements View.OnClickLis
 
         //禁止评论，隐藏评论框及评论按钮
         if (data.getArticle().getComment_level() == 0) {
+            mFyContainer.setVisibility(View.GONE);
             mMenuComment.setVisibility(View.GONE);
             mTvCommentsNum.setVisibility(View.GONE);
         } else {
+            mFyContainer.setVisibility(View.VISIBLE);
             mMenuComment.setVisibility(View.VISIBLE);
             if (!TextUtils.isEmpty(data.getArticle().getComment_count_general())) {
                 mTvCommentsNum.setVisibility(View.VISIBLE);
@@ -215,7 +237,7 @@ public class BrowserLinkActivity extends BaseActivity implements View.OnClickLis
 
     private Bundle bundle;
 
-    @OnClick({R2.id.iv_top_bar_back, R2.id.iv_top_share, R2.id.menu_comment, R2.id.menu_prised, R2.id.menu_setting})
+    @OnClick({R2.id.iv_top_bar_back, R2.id.iv_top_share, R2.id.menu_comment, R2.id.menu_prised, R2.id.menu_setting, R2.id.tv_comment})
     public void onClick(View view) {
         if (ClickTracker.isDoubleClick()) return;
 
@@ -352,6 +374,44 @@ public class BrowserLinkActivity extends BaseActivity implements View.OnClickLis
                         .send();
             }
             MoreDialog.newInstance(mNewsDetail).show(getSupportFragmentManager(), "MoreDialog");
+            //评论框
+        } else if (view.getId() == R.id.tv_comment) {
+            if (mNewsDetail != null && mNewsDetail.getArticle() != null) {
+                //进入评论编辑页面(不针对某条评论)
+                new Analytics.AnalyticsBuilder(getActivity(), "800002", "800002")
+                        .setEvenName("点击评论输入框")
+                        .setObjectID(mNewsDetail.getArticle().getMlf_id() + "")
+                        .setObjectName(mNewsDetail.getArticle().getDoc_title())
+                        .setObjectType(ObjectType.NewsType)
+                        .setClassifyID(mNewsDetail.getArticle().getChannel_id())
+                        .setClassifyName(mNewsDetail.getArticle().getChannel_name())
+                        .setPageType("新闻详情页")
+                        .setOtherInfo(Analytics.newOtherInfo()
+                                .put("relatedColumn", mNewsDetail.getArticle().getColumn_id() + "")
+                                .put("subject", "")
+                                .toString())
+                        .setSelfObjectID(mNewsDetail.getArticle().getId() + "")
+                        .build()
+                        .send();
+
+                //评论发表成功
+                Analytics analytics = new Analytics.AnalyticsBuilder(getActivity(), "A0023", "A0023")
+                        .setEvenName("发表评论，且发送成功")
+                        .setObjectID(mNewsDetail.getArticle().getMlf_id() + "")
+                        .setObjectName(mNewsDetail.getArticle().getDoc_title())
+                        .setObjectType(ObjectType.NewsType)
+                        .setClassifyID(mNewsDetail.getArticle().getChannel_id())
+                        .setClassifyName(mNewsDetail.getArticle().getChannel_name())
+                        .setPageType("新闻详情页")
+                        .setOtherInfo(Analytics.newOtherInfo()
+                                .put("relatedColumn", mNewsDetail.getArticle().getColumn_id() + "")
+                                .put("subject", "")
+                                .toString())
+                        .setSelfObjectID(mNewsDetail.getArticle().getId() + "")
+                        .build();
+                //进入评论编辑页面(不针对某条评论)
+                CommentWindowDialog.newInstance(new CommentDialogBean(String.valueOf(mNewsDetail.getArticle().getId()))).setWMData(analytics).show(getSupportFragmentManager(), "CommentWindowDialog");
+            }
         }
     }
 
@@ -387,10 +447,65 @@ public class BrowserLinkActivity extends BaseActivity implements View.OnClickLis
         }).setTag(this).exe(mArticleId, true);
     }
 
+
+//    /**
+//     * 分享回调
+//     *
+//     * @param requestCode
+//     * @param resultCode
+//     * @param data
+//     */
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        UMShareAPI.get(UIUtils.getApp()).onActivityResult(requestCode, resultCode, data);
+//
+//    }
+
+    /**
+     * 处理上下移动监听
+     *
+     * @param ev
+     * @return
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (mTouchSlopHelper != null)
+            mTouchSlopHelper.onTouchEvent(ev);
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private final Interpolator mInterpolator = new AccelerateDecelerateInterpolator();
+
+    /**
+     * @param isUp 控制底部floorBar
+     */
+    @Override
+    public void onTouchSlop(boolean isUp) {
+        int translationY = !isUp ? 0 : mFloorBar.getHeight() + getFloorBarMarginBottom();
+        mFloorBar.animate().setInterpolator(mInterpolator)
+                .setDuration(200)
+                .translationY(translationY);
+    }
+
+    /**
+     * @return 获取底部栏间距
+     */
+    private int getFloorBarMarginBottom() {
+        int marginBottom = 0;
+        final ViewGroup.LayoutParams layoutParams = mFloorBar.getLayoutParams();
+        if (layoutParams instanceof ViewGroup.MarginLayoutParams) {
+            marginBottom = ((ViewGroup.MarginLayoutParams) layoutParams).bottomMargin;
+        }
+        return marginBottom;
+    }
+
+
     /**
      * 显示撤稿页面
      */
     private void showEmptyNewsDetail() {
+        mFloorBar.setVisibility(View.GONE);
         mView.setVisibility(View.VISIBLE);
         topBarHolder.getShareView().setVisibility(View.GONE);
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();

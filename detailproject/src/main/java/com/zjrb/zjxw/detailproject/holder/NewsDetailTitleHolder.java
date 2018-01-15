3260a2a8b309d9548e4830564870dceb36e987fb
汪林杰 <wljie@8531.cn>
2018-01-15 09:29:59 +0800

    添加详情页阅读数

--- detailproject/src/main/java/com/zjrb/zjxw/detailproject/holder/NewsDetailTitleHolder.java
+++ detailproject/src/main/java/com/zjrb/zjxw/detailproject/holder/NewsDetailTitleHolder.java
@@ -22,9 +22,6 @@ import com.zjrb.zjxw.detailproject.R;
 import com.zjrb.zjxw.detailproject.R2;
 import com.zjrb.zjxw.detailproject.bean.DraftDetailBean;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -46,6 +43,8 @@ public class NewsDetailTitleHolder extends BaseRecyclerViewHolder<DraftDetailBea
     TextView mTvChannelName;
     @BindView(R2.id.tv_time)
     TextView mTvTime;
+    @BindView(R2.id.tv_read_num)
+    TextView mTvReadNum;
 
     public NewsDetailTitleHolder(ViewGroup parent) {
         super(UIUtils.inflate(R.layout.module_detail_layout_top, parent, false));
@@ -57,7 +56,7 @@ public class NewsDetailTitleHolder extends BaseRecyclerViewHolder<DraftDetailBea
         itemView.setOnClickListener(null);
 
         //顶部焦点图(可以不填写)
-        if (!TextUtils.isEmpty(mData.getArticle().getArticle_pic())) {
+        if (mData != null && mData.getArticle() != null && !TextUtils.isEmpty(mData.getArticle().getArticle_pic())) {
             mIvTopBg.setVisibility(View.VISIBLE);
             GlideApp.with(mIvTopBg).load(mData.getArticle().getArticle_pic()).apply(AppGlideOptions.bigOptions()).centerCrop().into(mIvTopBg);
         } else {
@@ -65,12 +64,12 @@ public class NewsDetailTitleHolder extends BaseRecyclerViewHolder<DraftDetailBea
         }
 
         //标题(必填)
-        if (mData.getArticle().getDoc_title() != null) {
+        if (mData != null && mData.getArticle() != null && mData.getArticle().getDoc_title() != null) {
             mTvTitle.setText(mData.getArticle().getDoc_title());
         }
 
         //来源及记者(发稿允许不填写)
-        if (TextUtils.isEmpty(mData.getArticle().getSource()) && TextUtils.isEmpty(mData.getArticle().getAuthor())) {
+        if (mData != null && mData.getArticle() != null && TextUtils.isEmpty(mData.getArticle().getSource()) && TextUtils.isEmpty(mData.getArticle().getAuthor())) {
             mTvReporter.setVisibility(View.GONE);
         } else {
             String source = mData.getArticle().getSource();
@@ -82,12 +81,19 @@ public class NewsDetailTitleHolder extends BaseRecyclerViewHolder<DraftDetailBea
         }
 
         //稿件发布时间/栏目名称(发稿允许不填写)
-        if (!TextUtils.isEmpty(mData.getArticle().getSource_channel_name()) && !TextUtils.isEmpty(mData.getArticle().getSource_channel_id())) {
+        if (mData != null && mData.getArticle() != null && !TextUtils.isEmpty(mData.getArticle().getSource_channel_name()) && !TextUtils.isEmpty(mData.getArticle().getSource_channel_id())) {
             mTvTime.setText(TimeUtils.getTime(mData.getArticle().getPublished_at(), C.DATE_FORMAT_1) + "  |");
             mTvChannelName.setText(mData.getArticle().getSource_channel_name());
         } else {
             mTvTime.setText(TimeUtils.getTime(mData.getArticle().getPublished_at(), C.DATE_FORMAT_1));
             mTvChannelName.setVisibility(View.GONE);
+        }
+
+        if (mData != null && mData.getArticle() != null && !TextUtils.isEmpty(mData.getArticle().getRead_count_general())) {
+            mTvReadNum.setVisibility(View.VISIBLE);
+            mTvReadNum.setText(mData.getArticle().getRead_count_general());
+        } else {
+            mTvReadNum.setVisibility(View.INVISIBLE);
         }
     }
 
@@ -96,7 +102,7 @@ public class NewsDetailTitleHolder extends BaseRecyclerViewHolder<DraftDetailBea
     @OnClick({R2.id.tv_channel_name})
     public void onClick(View view) {
         if (ClickTracker.isDoubleClick()) return;
-        if(mData != null && mData.getArticle() != null){
+        if (mData != null && mData.getArticle() != null) {
             new Analytics.AnalyticsBuilder(itemView.getContext(), "800012", "800012")
                     .setEvenName("点击稿件标题下频道名称")
                     .setObjectID(mData.getArticle().getChannel_id())
@@ -106,7 +112,7 @@ public class NewsDetailTitleHolder extends BaseRecyclerViewHolder<DraftDetailBea
                     .setClassifyName(mData.getArticle().getSource_channel_name())
                     .setPageType("新闻详情页")
                     .setOtherInfo(Analytics.newOtherInfo()
-                            .put("relatedColumn", mData.getArticle().getColumn_id()+"")
+                            .put("relatedColumn", mData.getArticle().getColumn_id() + "")
                             .put("subject", "")
                             .toString())
                     .setSelfObjectID(mData.getArticle().getId() + "")

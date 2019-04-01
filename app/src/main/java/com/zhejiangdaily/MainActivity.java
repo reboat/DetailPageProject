65aabff094b60d335fe110d1de116707fdf292df
汪林杰 <190785060@qq.com>
2019-04-01 17:24:01 +0800

    修改链接稿

--- app/src/main/java/com/zhejiangdaily/MainActivity.java
+++ app/src/main/java/com/zhejiangdaily/MainActivity.java
@@ -41,9 +41,9 @@ public class MainActivity extends DailyActivity implements View.OnClickListener 
 //        Nav.with(this).to("https://zj.zjol.com.cn/subject.html?id=1114462");
 //            Nav.with(this).toPath("/news/SpecialMoreActivity");
 
-        Nav.with(this).to("https://zj.zjol.com.cn/video.html?id=1152960");
+//        Nav.with(this).to("https://zj.zjol.com.cn/video.html?id=1152960");
 //        Nav.with(this).to("www.baidu.com");
-//        Nav.with(this).to("https://zj.zjol.com.cn/news.html?id=1152980");
+        Nav.with(this).to("https://zj.zjol.com.cn/news.html?id=1152980");
 //        Nav.with(this).to("https://zj.zjol.com.cn/subject.html?id=1146710"); // 741334
 //        Nav.with(this).to("https://zj.zjol.com.cn/subject.html?id=256620"); // 741791
 //        Nav.with(this).to("https://zj.zjol.com.cn/subject.html?id=785088"); // 741791

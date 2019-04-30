384fb1b38617d0c2b9ce857f8af799bcb5af8fe5
sishuqun <sisq@8531.cn>
2019-04-30 15:05:02 +0800

    MoreDialogLink分享埋点修改

--- detailproject/src/main/java/com/zjrb/zjxw/detailproject/utils/MoreDialogLink.java
+++ detailproject/src/main/java/com/zjrb/zjxw/detailproject/utils/MoreDialogLink.java
@@ -485,32 +485,32 @@ public class MoreDialogLink extends BaseDialogFragment {
             if (share_media == SHARE_MEDIA.WEIXIN) {
                 WMCode = "A0022";
                 UMCode = "60003";
-                eventName = "微信分享；微信分享成功";
+                eventName = "分享到微信成功";
                 eventDetail = "微信";
             } else if (share_media == SHARE_MEDIA.WEIXIN_CIRCLE) {
                 WMCode = "A0022";
                 UMCode = "60004";
-                eventName = "朋友圈分享；朋友圈分享成功";
+                eventName = "分享到朋友圈成功";
                 eventDetail = "朋友圈";
             } else if (share_media == SHARE_MEDIA.QQ) {
                 WMCode = "A0022";
                 UMCode = "800020";
-                eventName = "QQ分享；QQ分享成功";
+                eventName = "分享到QQ成功";
                 eventDetail = "QQ";
             } else if (share_media == SHARE_MEDIA.SINA) {
                 WMCode = "A0022";
                 UMCode = "60001";
-                eventName = "微博分享；微博分享成功";
+                eventName = "分享到微博成功";
                 eventDetail = "微博";
             } else if (share_media == SHARE_MEDIA.QZONE) {
                 WMCode = "A0022";
                 UMCode = "800019";
-                eventName = "QQ空间分享;QQ空间分享成功";
+                eventName = "分享到QQ空间成功";
                 eventDetail = "QQ空间";
             } else if (share_media == SHARE_MEDIA.DINGTALK) { // 钉钉
                 WMCode = "A0022";
                 UMCode = "800032";
-                eventName = "钉钉分享;钉钉分享成功";
+                eventName = "分享到钉钉成功";
                 eventDetail = "钉钉";
             }
             if (!TextUtils.isEmpty(mBeanShare.getAnalyticsBean().getPageType()) && mBeanShare.getAnalyticsBean().getPageType().equals("官员页面")) {

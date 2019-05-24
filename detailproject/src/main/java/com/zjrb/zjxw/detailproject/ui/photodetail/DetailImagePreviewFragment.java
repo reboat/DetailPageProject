53d64c4b88f9faf7d36e0c419412b68799677d78
李欣科 <lixink@8531.cn>
2019-05-24 11:25:05 +0800

    图集预览埋点

--- detailproject/src/main/java/com/zjrb/zjxw/detailproject/ui/photodetail/DetailImagePreviewFragment.java
+++ detailproject/src/main/java/com/zjrb/zjxw/detailproject/ui/photodetail/DetailImagePreviewFragment.java
@@ -18,7 +18,7 @@ public class DetailImagePreviewFragment extends ImagePreviewFragment {
     private Analytics mAnalytics;
 
 
-    public static DetailImagePreviewFragment newInstance(DraftDetailBean.ArticleBean bean,String url, boolean isFromAtlas, int mlfid) {
+    public static DetailImagePreviewFragment newInstance(DraftDetailBean.ArticleBean bean, String url, boolean isFromAtlas, int mlfid) {
         DetailImagePreviewFragment fragment = new DetailImagePreviewFragment();
 
         Bundle args = new Bundle();
@@ -33,43 +33,41 @@ public class DetailImagePreviewFragment extends ImagePreviewFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
         Bundle args = getArguments();
-        if (args != null) {
+        if (args != null && mBean==null) {
             mBean = (DraftDetailBean.ArticleBean) args.getSerializable("bean");
         }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mBean != null) {
-            mAnalytics = new Analytics.AnalyticsBuilder(getContext(), "A0010", "PicturePageStay", true)
-                    .name("图片浏览（左右滑动）")
-                    .selfObjectID(String.valueOf(mBean.getId()))
-                    .columnID(String.valueOf(mBean.getColumn_id()))
-                    .classShortName(mBean.getChannel_name())
-                    .objectShortName(mBean.getDoc_title())
-                    .classID(mBean.getChannel_id())
-                    .pageType("图集详情页")
-                    .ilurl(mBean.getUrl())
-                    .seObjectType(ObjectType.C01)
-                    .objectID(String.valueOf(mBean.getMlf_id()))
-                    .columnName(mBean.getColumn_name())
-                    .selfNewsID(String.valueOf(mBean.getId()))
-                    .pubUrl(mBean.getUrl())
-                    .selfChannelID(mBean.getChannel_id())
-                    .newsID(String.valueOf(mBean.getMlf_id()))
-                    .newsTitle(mBean.getDoc_title())
-                    .channelName(mBean.getChannel_name())
-                    .build();
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if(mAnalytics!=null){
-            mAnalytics.sendWithDuration();
+        if (isVisibleToUser) {
+            if (mBean != null) {
+                mAnalytics = new Analytics.AnalyticsBuilder(getContext(), "A0010", "PicturePageStay", true)
+                        .name("图片浏览（左右滑动）")
+                        .selfObjectID(String.valueOf(mBean.getId()))
+                        .columnID(String.valueOf(mBean.getColumn_id()))
+                        .classShortName(mBean.getChannel_name())
+                        .objectShortName(mBean.getDoc_title())
+                        .classID(mBean.getChannel_id())
+                        .pageType("图集详情页")
+                        .ilurl(mBean.getUrl())
+                        .seObjectType(ObjectType.C01)
+                        .objectID(String.valueOf(mBean.getMlf_id()))
+                        .columnName(mBean.getColumn_name())
+                        .selfNewsID(String.valueOf(mBean.getId()))
+                        .pubUrl(mBean.getUrl())
+                        .selfChannelID(mBean.getChannel_id())
+                        .newsID(String.valueOf(mBean.getMlf_id()))
+                        .newsTitle(mBean.getDoc_title())
+                        .channelName(mBean.getChannel_name())
+                        .build();
+            }
+        } else {
+            if (mAnalytics != null) {
+                mAnalytics.sendWithDuration();
+            }
         }
     }
 }
--- detailproject/src/main/java/com/zjrb/zjxw/detailproject/ui/photodetail/ImageMoreFragment.java
+++ detailproject/src/main/java/com/zjrb/zjxw/detailproject/ui/photodetail/ImageMoreFragment.java
@@ -139,33 +139,41 @@ public class ImageMoreFragment extends DailyFragment {
     private Analytics mAnalytics;
 
     @Override
-    public void onResume() {
-        super.onResume();
-        DraftDetailBean.ArticleBean articleBean = mBean.getArticle();
-        mAnalytics = new Analytics.AnalyticsBuilder(getContext(), "A0010", "PicturePageStay", true)
-                .name("打开“更多图集”页面")
-                .selfObjectID(String.valueOf(articleBean.getId()))
-                .columnID(String.valueOf(articleBean.getColumn_id()))
-                .classShortName(articleBean.getChannel_name())
-                .objectShortName(articleBean.getDoc_title())
-                .classID(articleBean.getChannel_id())
-                .pageType("更多图集页")
-                .ilurl(articleBean.getUrl())
-                .seObjectType(ObjectType.C01)
-                .objectID(String.valueOf(articleBean.getMlf_id()))
-                .columnName(articleBean.getColumn_name())
-                .selfNewsID(String.valueOf(articleBean.getId()))
-                .pubUrl(articleBean.getUrl())
-                .selfChannelID(articleBean.getChannel_id())
-                .newsID(String.valueOf(articleBean.getMlf_id()))
-                .newsTitle(articleBean.getDoc_title())
-                .channelName(articleBean.getChannel_name())
-                .build();
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser) {
+            DraftDetailBean.ArticleBean articleBean = mBean.getArticle();
+            mAnalytics = new Analytics.AnalyticsBuilder(getContext(), "A0010", "PicturePageStay", true)
+                    .name("打开“更多图集”页面")
+                    .selfObjectID(String.valueOf(articleBean.getId()))
+                    .columnID(String.valueOf(articleBean.getColumn_id()))
+                    .classShortName(articleBean.getChannel_name())
+                    .objectShortName(articleBean.getDoc_title())
+                    .classID(articleBean.getChannel_id())
+                    .pageType("更多图集页")
+                    .ilurl(articleBean.getUrl())
+                    .seObjectType(ObjectType.C01)
+                    .objectID(String.valueOf(articleBean.getMlf_id()))
+                    .columnName(articleBean.getColumn_name())
+                    .selfNewsID(String.valueOf(articleBean.getId()))
+                    .pubUrl(articleBean.getUrl())
+                    .selfChannelID(articleBean.getChannel_id())
+                    .newsID(String.valueOf(articleBean.getMlf_id()))
+                    .newsTitle(articleBean.getDoc_title())
+                    .channelName(articleBean.getChannel_name())
+                    .build();
+        } else {
+            if (mAnalytics != null) {
+                mAnalytics.sendWithDuration();
+            }
+        }
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        mAnalytics.sendWithDuration();
+        if(mAnalytics!=null){
+            mAnalytics.sendWithDuration();
+        }
     }
 }
